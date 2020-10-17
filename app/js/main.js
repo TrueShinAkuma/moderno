1d53a307df92fba597faa7b71ab0b380b9e4c324
TrueShinAkuma <shinakuma.work@gmail.com>
2020-10-17 05:25:46 +0500

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,31 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".js-range-slider").ionRangeSlider({
@@ -18,38 +43,48 @@ $(function () {
     max: 1000,
     from: 0,
     to: 600,
-    prefix: "$"
+    prefix: "$",
   });
 
-  $('.icon-th-list').on('click', function(){
-    $('.product__item').addClass('list')
-    $('.icon-th-list').addClass('active')
-    $('.icon-th-large').removeClass('active')
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").addClass("active");
+    $(".icon-th-large").removeClass("active");
   });
 
-  $('.icon-th-large').on('click', function(){
-    $('.product__item').removeClass('list')
-    $('.icon-th-large').addClass('active')
-    $('.icon-th-list').removeClass('active')
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
   });
 
-  $('.menu__btn').on('click', function(){
-    $('.menu__list').slideToggle();
+  $(".menu__btn").on("click", function () {
+    $(".menu__list").slideToggle();
   });
 
-  $('.header__btn-menu').on('click', function(){
-    $('.header__box').toggleClass('active');
+  $(".header__btn-menu").on("click", function () {
+    $(".header__box").toggleClass("active");
   });
-  
-  $('.product-one__tabs .tab, .settings__tabs .tabs').on('click', function(event) {
-    var id = $(this).attr('data-id');
-      $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
-      $(this).addClass('active');
-      $('#'+id).addClass('active-tab').fadeIn();
-      return false;
-    });
 
+  $(".product-one__tabs .tab, .settings__tabs .tabs").on("click", function (
+    event
+  ) {
+    var id = $(this).attr("data-id");
+    $(".product-one__tabs, .settings__tabs")
+      .find(".tab-item")
+      .removeClass("active-tab")
+      .hide();
+    $(".product-one__tabs .tabs, .settings__tabs .tabs")
+      .find(".tab")
+      .removeClass("active");
+    $(this).addClass("active");
+    $("#" + id)
+      .addClass("active-tab")
+      .fadeIn();
+    return false;
+  });
+
+  $('input[type="file"], select').styler();
 
   var mixer = mixitup(".products__inner-box");
 });
